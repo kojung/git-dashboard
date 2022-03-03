4d5d67c40b925b1c5f7f5d66ee7ec718166e9d91
Jung Ko <kojung@gmail.com>
2022-03-02 20:55:34 -0800

    0.1.4 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def load_readme():
 
 setup(
     name='git_dashboard',
-    version='0.1.3',
+    version='0.1.4',
     description="Git dashboard",
     author="Jung Ko",
     author_email="kojung@gmail.com",
