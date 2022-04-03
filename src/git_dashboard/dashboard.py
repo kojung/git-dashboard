34eccbbf1b87c0b7843156981904b76f3a41269f
Jung Ko <kojung@gmail.com>
2022-04-02 19:13:43 -0700

    moved refresh to a separate thread

--- src/git_dashboard/dashboard.py
+++ src/git_dashboard/dashboard.py
@@ -23,10 +23,14 @@ import os
 import sys
 import signal
 from pathlib import Path
-import functools
 import argparse
+import time
 
-from PySide6.QtCore import QTimer
+from PySide6.QtCore import (
+    Signal,
+    QThread,
+)
+
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -50,13 +54,21 @@ def sigint_handler(*args): # pylint: disable=unused-argument
     """Handler for the SIGINT signal."""
     QApplication.quit()
 
-def refresh(view, config):
-    """refresh repo status"""
-    groups = load_configuration(config)
-    for name, model in view.models.items():
-        model.layoutAboutToBeChanged.emit()        # pylint: disable=no-member
-        model.group = groups[name]
-        model.layoutChanged.emit()                 # pylint: disable=no-member
+class RefreshThread(QThread):
+    """Separate thread used to query git repos in the background"""
+    ready = Signal(object)   # Signal must be class, not instance member
+    def __init__(self, config, refresh):
+        """constructor"""
+        super().__init__()
+        self.config  = config
+        self.refresh = refresh
+
+    def run(self):
+        """thread run method"""
+        while True:
+            groups = load_configuration(self.config)
+            self.ready.emit(groups)
+            time.sleep(self.refresh)
 
 def parser():
     """argument parser"""
@@ -86,12 +98,17 @@ def main():
     view   = GroupsView(groups, args)
     window = MainWindow(view)
 
-    # status refresh rate
-    refresh_callback = functools.partial(refresh, view=view, config=args.config)
+    def refresh_func(groups):
+        """refresh repo status"""
+        for name, model in view.models.items():
+            model.layoutAboutToBeChanged.emit()  # pylint: disable=no-member
+            model.group = groups[name]
+            model.layoutChanged.emit()           # pylint: disable=no-member
 
-    timer = QTimer()
-    timer.start(args.refresh * 1000)  # in milliseconds
-    timer.timeout.connect(refresh_callback)  # pylint: disable=no-member
+    # start refresh thread and connect it refresh_func
+    refresh_thread = RefreshThread(args.config, args.refresh)
+    refresh_thread.ready.connect(refresh_func)
+    refresh_thread.start()
 
     # resize primary window to 1/5 width + 1/2 height
     screen = app.primaryScreen()
